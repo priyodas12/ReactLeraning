af1b821eefb1feb8404e5de58c0b48e3500aa202
Priyobrato Das <priyodas12@gmail.com>
2019-03-25 11:16:13 +0530

    state initialized

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,14 @@
 import React from "react";
-const person = () => {
-  return <p>Random Person</p>;
+const person = props => {
+  return (
+    <div>
+      <p>
+        I am {props.name} and gender:{props.gender},
+        {Math.floor(Math.random() * 20)} years old!
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
 };
 
 export default person;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Person from "./Person/Person.js";
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <Person />
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: "abcd", gender: "male" },
+      { name: "defgs", gender: "male" },
+      { name: "hdge", gender: "female" }
+    ]
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+        <button>Switch App</button>
+        <Person
+          name={this.state.persons[0].name}
+          gender={this.state.persons[0].gender}
+        >
+          Hobby:Cricket
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          gender={this.state.persons[1].gender}
+        >
+          Hobby:Foorball
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          gender={this.state.persons[2].gender}
+        >
+          Hobby:Hocky
+        </Person>
+      </div>
+    );
+  }
 }
-
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
